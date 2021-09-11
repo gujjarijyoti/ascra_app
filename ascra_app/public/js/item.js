9e2a8d056b06362e9a1fa06ca4628e693cef61e2
umaepoch <umaepoch@consulting.in>
2021-09-11 18:32:18 +0530

    Item Script added

--- ascra_app/public/js/item.js
+++ ascra_app/public/js/item.js
@@ -1,24 +1,20 @@
 frappe.ui.form.on("Item", "before_save", function (frm, cdt, cdn) {
-    console.log("entered")
-    var iitem_details = locals[cdt][cdn];
-    var variant_of = iitem_details.variant_of;
-    var height = iitem_details.heigth;
-    var width = iitem_details.width;
-    var yields = iitem_details.yields;
-    var length = iitem_details.length;
-    var weight_per_unit = iitem_details.weight_per_unit;
-    var warranty_period = iitem_details.warranty_period;
-    var shelf_life_in_days = iitem_details.shelf_life_in_days;
+    //console.log("entered")
+    var item_details = locals[cdt][cdn];
+    var variant_of = item_details.variant_of;
+    var height = item_details.height;
+    var width = item_details.width;
+    var yields = item_details.yields;
+    var length = item_details.length;
     
-    //Getting UOM Conversion Detail(Child Table) Data
+    
+    //Calculating UOM Conversion (Child Table) Data
     if (variant_of != 'undefined') {
-        console.log("entered in uom")
-        $.each(iitem_details.uoms || [], function (i, item) {
-            console.log(iitem_details.item_code)
-            console.log(item.formula)
+        //console.log("entered in uom")
+        $.each(item_details.uoms || [], function (i, item) {
             if (item.formula) {
                 var final_formula =  item.formula
-                console.log(eval(final_formula)); 
+               // console.log(eval(final_formula)); 
                 item.conversion_factor = flt(eval(final_formula))
                 item.formula = item.formula
             }
